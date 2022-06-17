8ae503d3238073cbd6642d6fca329d8db72120e8
HaynesX <brandon@haynes.bio>
2022-06-17 21:46:30 +0100

    Changed settings_prod file

--- xdb_price_chat/settings/production.py
+++ xdb_price_chat/settings/production.py
@@ -124,7 +124,7 @@ DATABASES = {
         'NAME': os.environ['PROD_MYSQL_DATABASE_NAME'],
         'USER': os.environ['PROD_MYSQL_DATABASE_USER'],
         'PASSWORD': os.environ['PROD_MYSQL_DATABASE_PASSWORD'],
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
 }
