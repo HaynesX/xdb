93837e93f252e58cad90208fb2b8de7ca0ae3bdb
HaynesX <brandon@haynes.bio>
2022-06-17 23:05:08 +0100

    Changed db information for testing purposes

--- xdb_price_chat/settings/production.py
+++ xdb_price_chat/settings/production.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "xdbpricechat.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "xdbpricechat.com", "0.0.0.0"]
 
 CSRF_TRUSTED_ORIGINS = ["https://xdbpricechat.com"]
 # Application definition
@@ -172,7 +172,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
