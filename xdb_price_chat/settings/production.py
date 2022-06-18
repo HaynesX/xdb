2dfddc32dd7aa16e30761336595e0255bb4e7605
HaynesX <brandon@haynes.bio>
2022-06-18 22:46:40 +0100

    Changed.

--- xdb_price_chat/settings/production.py
+++ xdb_price_chat/settings/production.py
@@ -188,9 +188,9 @@ LOGOUT_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
